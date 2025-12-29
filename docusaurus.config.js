fbd7b4137844953abbf088b696c2f00a0aebc748
Ad1nn <buttadan745@gmail.com>
2025-12-29 12:16:27 +0500

    Change baseUrl to root for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://Ad1nn.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-Humanoid-Robotics-Ai-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
